e00d8cc0123329b8793332bcc704344f26c62e73
WizzOFF <maher_off@proton.me>
2025-03-17 16:17:13 +0100

    manipulate_files_using_fonctions.c: the pointer is initialized to NULL

---
+++ manipulate_files_using_functions.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+FILE* fopen(const char* nomDuFichier, const char* modeOuverture);
+
+int main(int argc, char *argv[])
+{
+    FILE* fichier = NULL;
+
+    return 0;
+}
