cd7d83afbad9066bf066f8c6c9de6b2d834f12ce
WizzOFF <maher_off@proton.me>
2025-03-17 17:07:19 +0100

    manipulate_files_using_fonctions.c: read a string with fgets

--- manipulate_files_using_functions.c
+++ manipulate_files_using_functions.c
@@ -68,7 +68,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 */
-
+/*
 int fgetc(FILE* pointeurDeFichier);
 
 int main(int argc, char *argv[])
@@ -86,6 +86,29 @@ int main(int argc, char *argv[])
             caractereActuel = fgetc(fichier); // On lit le caractère
             printf("%c", caractereActuel); // On l'affiche
         } while (caractereActuel != EOF); // On continue tant que fgetc n'a pas retourné EOF (fin de fichier)
+ 
+        fclose(fichier);
+    }
+ 
+    return 0;
+}
+*/
+
+char* fgets(char* chaine, int nbreDeCaracteresALire, FILE* pointeurSurFichier);
+
+#define TAILLE_MAX 1000 // Tableau de taille 1000
+ 
+int main(int argc, char *argv[])
+{
+    FILE* fichier = NULL;
+    char chaine[TAILLE_MAX] = ""; // Chaîne vide de taille TAILLE_MAX
+ 
+    fichier = fopen("test.txt", "r");
+ 
+    if (fichier != NULL)
+    {
+        fgets(chaine, TAILLE_MAX, fichier); // On lit maximum TAILLE_MAX caractères du fichier, on stocke le tout dans "chaine"
+        printf("%s", chaine); // On affiche la chaîne
  
         fclose(fichier);
     }
