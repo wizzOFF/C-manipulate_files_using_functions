0330d740ad05a80711e4359a3ece9de166e2eea1
WizzOFF <maher_off@proton.me>
2025-03-17 16:26:25 +0100

    manipulate_files_using_fonctions.c: add an error message

--- manipulate_files_using_functions.c
+++ manipulate_files_using_functions.c
@@ -9,5 +9,15 @@ int main(int argc, char *argv[])
 
     fichier = fopen("README.md", "r+");
 
+    if (fichier != NULL)
+    {
+        // On peut lire et écrire dans le fichier
+    }
+    else
+    {
+        // On affiche un message d'erreur si on veut
+        printf("Impossible d'ouvrir le fichier test.txt");
+    }
+    
     return 0;
 }
